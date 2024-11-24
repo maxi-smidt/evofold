3099d9ede2ce791c14dd57d7c9f2b570d6893dcf
msmidt <maxismidt@icloud.com>
2024-11-24 21:56:05 +0100

    [Feature #11] basic routing and components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import {StructureViewerComponent} from './features/structure-viewer/structure-viewer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'structure', component: StructureViewerComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
