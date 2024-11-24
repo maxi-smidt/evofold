4f2d2a33e0cf85c671840b083a16f8f405ddd38e
Maximilian Smidt <134879273+maxi-smidt@users.noreply.github.com>
2024-11-24 21:57:34 +0100

    [Feature #11] basic routing and components (#14)

---
+++ frontend/src/app/features/structure-viewer/structure-viewer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-structure-viewer',
+  imports: [],
+  templateUrl: './structure-viewer.component.html',
+  styleUrl: './structure-viewer.component.css'
+})
+export class StructureViewerComponent {
+
+}
