47b49b0207d0b45c5b6057ef5e555be6405a4e92
msmidt <maxismidt@icloud.com>
2024-11-29 20:21:38 +0100

    [NOISSUE] refactorings

--- poc/structure.py
+++ poc/structure.py
@@ -5,7 +5,7 @@ import residue_constants as rc
 from dataclasses import dataclass, field
 from itertools import count
 from scipy.optimize import minimize
-from typing import List, Literal, Union
+from typing import List, Literal, Union, TextIO
 
 
 @dataclass
@@ -20,7 +20,7 @@ class AtomStructure:
 
 @dataclass()
 class AminoAcidStructure:
-    sequence_position: int
+    sequence_id: int
     one_letter_code: str
     previous_aa: Union['AminoAcidStructure', None]
     three_letter_code: str = field(init=False)
@@ -80,9 +80,12 @@ class ProteinStructure:
             previous_aa = current_aa
 
     def to_cif(self, file_name: str):
-        self._print_cif_positions(file_name)
+        with open(file_name, 'w') as f:
+            self._print_cif_header(f)
+            self._print_cif_positions(f)
 
-    def _print_cif_positions(self, file_name: str):
+    def _print_cif_positions(self, file: TextIO):
+        self._print_cif_header(file)
         atom_header = [
             'loop_', '_atom_site.group_PDB', '_atom_site.id', '_atom_site.type_symbol', '_atom_site.label_atom_id',
             '_atom_site.label_alt_id', '_atom_site.label_comp_id', '_atom_site.label_asym_id',
@@ -93,38 +96,43 @@ class ProteinStructure:
             '_atom_site.pdbx_PDB_model_num'
         ]
 
-        with open(file_name, 'w') as f:
-            f.write('\n'.join(atom_header))
-            atom_nr = count(start=0)
-            aa_nr = count(start=0)
-            for aa in self.amino_acids:
-                seq_id = next(aa_nr)
-                for atom in aa.atoms:
-                    data = [
-                        'ATOM',                 # group_PDB (here always ATOM)
-                        f"{next(atom_nr)}",     # id
-                        atom.atom_id[0],        # type_symbol (e.g. N, O, C)
-                        atom.atom_id,           # label_atom_id (e.g. CA, O, H1)
-                        '.',                    # label_alt_id (always .)
-                        aa.three_letter_code,   # label_comp_id
-                        'C',                    # label_asym_id (always C)
-                        '3',                    # label_entity_id (always 3)
-                        f"{seq_id}",            # label_seq_id
-                        '?',                    # pdbx_PDB_ins_code (always ?)
-                        f"{atom.x:.3f}",        # Cartn_x
-                        f"{atom.y:.3f}",        # Cartn_y
-                        f"{atom.z:.3f}",        # Cartn_z
-                        '1.00',                 # occupancy (always 1.00)
-                        '0.00',                 # B_iso_or_equiv (always 0.00)
-                        '?',                    # pdbx_formal_charge (always ?)
-                        f"{seq_id}",            # auth_seq_id (like label_seq_id)
-                        aa.three_letter_code,   # auth_comp_id
-                        'A',                    # auth_asym_id (always A)
-                        f"{atom.atom_id}",      # auth_atom_id (like label_atom_id)
-                        '1'                     # pdbx_PDB_model_num (1 because we always have only one chain)
-                    ]
-                    f.write('\n' + '\t'.join(data))
+        file.write('\n'.join(atom_header))
+        atom_nr = count(start=0)
+        aa_nr = count(start=0)
+        for aa in self.amino_acids:
+            seq_id = next(aa_nr)
+            for atom in aa.atoms:
+                data = [
+                    'ATOM',                 # group_PDB (here always ATOM)
+                    f"{next(atom_nr)}",     # id
+                    atom.atom_id[0],        # type_symbol (e.g. N, O, C)
+                    atom.atom_id,           # label_atom_id (e.g. CA, O, H1)
+                    '.',                    # label_alt_id (always .)
+                    aa.three_letter_code,   # label_comp_id
+                    'C',                    # label_asym_id (always C)
+                    '3',                    # label_entity_id (always 3)
+                    f"{seq_id}",            # label_seq_id
+                    '?',                    # pdbx_PDB_ins_code (always ?)
+                    f"{atom.x:.3f}",        # Cartn_x
+                    f"{atom.y:.3f}",        # Cartn_y
+                    f"{atom.z:.3f}",        # Cartn_z
+                    '1.00',                 # occupancy (always 1.00)
+                    '0.00',                 # B_iso_or_equiv (always 0.00)
+                    '?',                    # pdbx_formal_charge (always ?)
+                    f"{seq_id}",            # auth_seq_id (like label_seq_id)
+                    aa.three_letter_code,   # auth_comp_id
+                    'A',                    # auth_asym_id (always A)
+                    f"{atom.atom_id}",      # auth_atom_id (like label_atom_id)
+                    '1'                     # pdbx_PDB_model_num (1 because we always have only one chain)
+                ]
+                file.write('\n' + '\t'.join(data))
 
+    def _print_cif_header(self, file: TextIO):
+        file.write(f'data_EvoFold_{self.sequence}\n')
+
+    @staticmethod
+    def _print_cif_seperator(file: TextIO):
+        file.write('#\n')
 
 
 x = ProteinStructure("PEPTIDE")
