7784a8565458c1f0ffebc72c5021dae6190542b6
Maximilian Smidt <134879273+maxi-smidt@users.noreply.github.com>
2024-12-01 01:01:52 +0100

    [Feature #12] Dockerfiles, docker compose (#16)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
-import {StructureViewerComponent} from './features/structure-viewer/structure-viewer.component';
+import { StructureViewerComponent } from './features/structure-viewer/structure-viewer.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
