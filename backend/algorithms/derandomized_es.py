b6324d6b963ea19061f6b91dcefc2f84e3de9436
msmidt <maxismidt@icloud.com>
2025-04-14 20:11:33 -0400

    tracked global best and refactored es

--- backend/algorithms/derandomized_es.py
+++ backend/algorithms/derandomized_es.py
@@ -2,7 +2,6 @@ import numpy as np
 
 from multiprocessing import get_context
 from overrides import overrides
-from typing import Callable, List
 
 from backend.algorithms.es import ES
 from backend.algorithms.params.derandomized_es_params import DerandomizedESParams
@@ -24,39 +23,30 @@ class DerandomizedES(ES):
         return Protein(sequence, self._params.force_field, flat_angles=angles)
 
     @overrides
-    def run(self, sequence: str, callback: Callable[[int, Protein, float, bool], None]=None, callback_frequency: int=1) -> Protein:
-        generation:     int           = 0
-        sigma:          float         = self._params.sigma
-        population:     List[Protein] = self._create_initial_population(sequence)
-        best_offspring: Protein       = min(population, key=lambda p: p.fitness)
-        l:              int           = len(sequence) * 2
-        s:              np.array      = np.zeros(l)
+    def run(self, sequence: str, callback: ES.Callback=None, callback_frequency: int=1) -> Protein:
+        self._initialize_run(sequence, callback_frequency)
+        sigma:         float    = self._params.sigma
+        genome_length: int      = len(sequence) * 2
+        s:             np.array = np.zeros(genome_length)
 
         with get_context("spawn").Pool() as pool:
-            while generation < self._params.generations:
-                generation += 1
+            while self._generation < self._params.generations:
+                self._generation += 1
+                children = self._population if self._params.plus_selection else []
 
-                mean = sum(p.angles_flat for p in population) / len(population)
-                children = population if self._params.plus_selection else []
+                mean = sum(p.angles_flat for p in self._population) / len(self._population)
 
                 results = pool.starmap(self._process_child, [(sequence, mean, sigma) for _ in range(self._params.children_size)])
                 children.extend(results)
 
-                population = self._make_selection(children)
+                if self._finalize_generation(children, callback, sigma):
+                    return self._global_best_offspring
 
-                z_mean = sum((p.angles_flat - mean) / sigma  for p in population) / len(population)
+                z_mean = sum((p.angles_flat - mean) / sigma  for p in self._population) / len(self._population)
 
                 s = (1 - self._params.alpha) * s + np.sqrt(self._params.alpha * (2 - self._params.alpha) * self._params.population_size) * z_mean
 
-                sigma = sigma * np.exp((np.linalg.norm(s) ** 2 - l) / (2 * self._params.tau * l))
+                sigma = sigma * np.exp((np.linalg.norm(s) ** 2 - genome_length) / (2 * self._params.tau * genome_length))
 
-                best_offspring = min(population, key=lambda p: p.fitness)
 
-                is_premature_termination = self._is_premature_termination(best_offspring)
-
-                if callback is not None and generation % callback_frequency == 0:
-                    callback(generation, best_offspring, sigma, is_premature_termination or (generation >= self._params.generations))
-                if is_premature_termination:
-                    return best_offspring
-
-        return best_offspring
+        return self._global_best_offspring
--- backend/algorithms/es.py
+++ backend/algorithms/es.py
@@ -7,6 +7,8 @@ from backend.structure.protein import Protein
 
 
 class ES(ABC):
+    Callback = Callable[[int, Protein, float, bool], None]
+
     def __init__(self, params: ESParams):
         self._params = params
         self._previous_best = None
@@ -15,6 +17,27 @@ class ES(ABC):
     @abstractmethod
     def run(self, sequence: str, callback: Callable[[int, Protein, float, bool], None] = None, callback_frequency: int = 1) -> Protein:
         ...
+
+    def _initialize_run(self, sequence: str, callback_frequency) -> None:
+        self._generation:            int           = 0
+        self._population:            List[Protein] = self._create_initial_population(sequence)
+        self._local_best_offspring:  Protein       = min(self._population, key=lambda p: p.fitness)
+        self._global_best_offspring: Protein       = self._local_best_offspring
+        self._callback_frequency:    int           = callback_frequency
+
+    def _finalize_generation(self, children: List[Protein], callback: Callback, sigma: float | None) -> bool:
+        self._population = self._make_selection(children)
+        self._local_best_offspring = min(self._population, key=lambda p: p.fitness)
+        self._global_best_offspring = min(self._local_best_offspring, self._global_best_offspring, key=lambda p: p.fitness)
+
+        is_premature_termination = self._is_premature_termination(self._local_best_offspring)
+
+        sigma = sigma or self._global_best_offspring.sigma
+        if callback is not None and self._generation % self._callback_frequency == 0:
+            callback(self._generation, self._local_best_offspring, sigma,
+                     is_premature_termination or (self._generation >= self._params.generations))
+
+        return is_premature_termination
 
     def _make_selection(self, children: List[Protein]) -> List[Protein]:
         fitness: Callable[[Protein], float] = lambda p: p.fitness
