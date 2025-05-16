e8c9f78b62853171f55dbfa49f5dffe89df2b1e5
msmidt <maxismidt@icloud.com>
2025-05-15 22:06:26 -0400

    [NOISSUE] gb collector

--- backend/run.py
+++ backend/run.py
@@ -2,6 +2,7 @@ import time
 from functools import partial
 
 import numpy as np
+import gc
 
 from backend.algorithms.params.self_adaptive_es_params import SelfAdaptiveESParams
 from backend.algorithms.self_adaptive_es import SelfAdaptiveES
@@ -24,7 +25,7 @@ def save_time(generation: int, protein: Protein, fitness: float, is_best: bool, 
 
 def measure_es(method: str, params: SelfAdaptiveESParams, sequence: str):
     global t
-    for i in range(20):
+    for i in range(2):
         t = time.time()
         run_times = []
         cb = partial(save_time, l=run_times)
@@ -36,8 +37,10 @@ def run_measurement(sequence: str):
     log(f"seq: {sequence}")
     log(f"starting amber n={len(sequence)}")
     measure_es('amber', SelfAdaptiveESParams(), sequence)
+    gc.collect()
     log(f"starting charmm n={len(sequence)}")
     measure_es('charmm', SelfAdaptiveESParams(force_field='charmm'), sequence)
+    gc.collect()
 
 def main():
     for n in [10, 30, 75, 100, 150]:
